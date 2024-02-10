cb8b5f7312f2ff1edde837a9450b181c972f640e
satyachandu11 <rockonchandu11@gmail.com>
2024-02-10 17:24:29 +0530

    Folders Stuctured, Routed to Dashboard Page, sidebar and main-section component styled created and attached.

---
+++ src/components/MainSection/MainSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MainSection() {
+  return (
+    <div className="main-section">
+      <p>main</p>
+    </div>
+  );
+}
+
+export default MainSection
---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Sidebar from '../components/Sidebar/Sidebar'
+import MainSection from '../components/MainSection/MainSection'
+
+function Dashboard() {
+  return (
+    <div className="App">
+        <Sidebar />
+        <MainSection />
+    </div>
+  )
+}
+
+export default Dashboard
